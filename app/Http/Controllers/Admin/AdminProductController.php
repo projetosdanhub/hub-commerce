73096c75124290fdfc9ca776a3bcfc615445d596
projetosdanhub <projetosdan.hub@gmail.com>
2026-09-03 20:12:45 -0300

    refactor: reutilizar limite de paginação

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Support\Http\Pagination;
 use App\Enums\ProductStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\SaveProductRequest;
@@ -45,7 +46,7 @@ class AdminProductController extends Controller
             }
         }
 
-        $limit = min(max($request->integer('limit', 15), 1), 100);
+        $limit = Pagination::perPage($request, 15);
         $produtos = $query->orderByDesc('id')->paginate($limit);
 
         return response()->json([
