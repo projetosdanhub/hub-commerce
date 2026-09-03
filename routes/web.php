bb199a274e087af77ba7dcd9872f189ef416a8cf
projetosdanhub <projetosdan.hub@gmail.com>
2026-09-03 14:39:14 -0300

    fix(api): corrigir regex da rota SPA

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,4 @@ use Illuminate\Support\Facades\Route;
 // A SPA nunca deve capturar URLs de API inexistentes.
 Route::get('/{any?}', function () {
     return view('app');
-})->where('any', '^(?!api(?:/|$)).*);
+})->where('any', '^(?!api(?:/|$)).*');
