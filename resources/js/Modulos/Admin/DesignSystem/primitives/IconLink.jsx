7fc6d724ab8c2275f9c60bcded9c13442cf12d68
projetosdanhub <projetosdan.hub@gmail.com>
2026-09-04 21:20:19 -0300

    feat(ui): adiciona primitive reutilizável

---
+++ resources/js/Modulos/Admin/DesignSystem/primitives/IconLink.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Tooltip } from './Tooltip';
+
+export const IconLink = ({
+  icon: Icon,
+  label,
+  className = '',
+  children,
+  ...props
+}) => (
+  <Tooltip content={label}>
+    <a
+      className={`hub-icon-button ${className}`}
+      aria-label={label}
+      {...props}
+    >
+      <Icon aria-hidden="true" size={18} strokeWidth={1.8} />
+      {children}
+    </a>
+  </Tooltip>
+);
