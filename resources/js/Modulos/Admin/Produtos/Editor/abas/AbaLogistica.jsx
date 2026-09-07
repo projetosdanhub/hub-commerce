c693cc8c1cbf12c9ecb7c077667ed9a65e74fad8
projetosdanhub <projetosdan.hub@gmail.com>
2026-09-07 13:02:44 -0300

    refactor(admin): remove superseded Portuguese products path

--- resources/js/Modulos/Admin/Produtos/Editor/abas/AbaLogistica.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ProductLogisticsForm } from '../../../Products/ProductLogisticsForm';
-
-export default function AbaLogistica({ p, setP }) {
-  return <ProductLogisticsForm product={p} onChange={setP} />;
-}
