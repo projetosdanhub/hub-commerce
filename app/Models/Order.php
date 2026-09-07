325cb1aff95c727b80b7ec954ccae032b00bb521
projetosdanhub <projetosdan.hub@gmail.com>
2026-09-07 12:21:42 -0300

    fix(payments): reforça contrato de idempotência

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,7 @@ use App\Enums\OrderStatus;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
