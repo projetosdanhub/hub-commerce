b48c01c904ff72416a8a78f10f9c2757b1af3aca
projetosdanhub <projetosdan.hub@gmail.com>
2026-09-07 13:02:41 -0300

    refactor(admin): remove superseded Portuguese products path

--- resources/js/Modulos/Admin/Produtos/Editor/abas/AbaFichaTecnica.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ProductSpecificationForm } from '../../../Products/ProductSpecificationForm';
-
-export default function AbaFichaTecnica({ p, setP }) {
-  return <ProductSpecificationForm product={p} onChange={setP} />;
-}
