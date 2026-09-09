493de32aa201a067b362a36d5b3e469c814487b4
projetosdanhub <projetosdan.hub@gmail.com>
2026-09-09 17:13:43 -0300

    fix: cache trusted proxy configuration

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
     |
     | This value is the name of your application, which will be used when the
     | framework needs to place the application's name in a notification or
-    | other UI elements where an application name needs to be displayed.
+    | other UI elements where the application's name needs to be displayed.
     |
     */
 
@@ -54,6 +54,18 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Trusted Proxies
+    |--------------------------------------------------------------------------
+    |
+    | Keep this value in configuration so it is available after config:cache.
+    | Use "*" only for a local, controlled HTTPS tunnel.
+    |
+    */
+
+    'trusted_proxies' => env('TRUSTED_PROXIES', ''),
+
 /*
     |--------------------------------------------------------------------------
     | Application Timezone
@@ -78,9 +90,9 @@ return [
     | Encryption Key
     |--------------------------------------------------------------------------
     |
-    | This key is utilized by Laravel's encryption services and should be set
+    | This key is used by the Illuminate encrypter service and should be set
     | to a random, 32 character string to ensure that all encrypted values
-    | are secure. You should do this prior to deploying the application.
+    | are secure. You should do this before deploying the application.
     |
     */
 
@@ -103,7 +115,7 @@ return [
     | manage Laravel's "maintenance mode" status. The "cache" driver will
     | allow maintenance mode to be controlled across multiple machines.
     |
-    | Supported drivers: "file", "cache"
+    | Supported: "file", "cache"
     |
     */
 
