a641c992a633a1f1ae63777ecee4e80f0aabbd17
projetosdanhub <projetosdan.hub@gmail.com>
2026-09-08 23:56:21 -0300

    feat(oauth): add Melhor Envio platform configuration

--- config/provider-connections.php
+++ config/provider-connections.php
@@ -29,6 +29,20 @@ return [
     ],
 
     /*
+    | O aplicativo OAuth é da plataforma. A loja só autoriza sua conta;
+    | Client ID, Secret e User-Agent ficam exclusivamente na infraestrutura.
+    */
+    'melhor_envio' => [
+        'client_id' => env('MELHOR_ENVIO_PLATFORM_CLIENT_ID'),
+        'client_secret' => env('MELHOR_ENVIO_PLATFORM_CLIENT_SECRET'),
+        'user_agent' => env('MELHOR_ENVIO_PLATFORM_USER_AGENT'),
+        'sandbox_authorization_url' => 'https://sandbox.melhorenvio.com.br/oauth/authorize',
+        'production_authorization_url' => 'https://www.melhorenvio.com.br/oauth/authorize',
+        'sandbox_token_url' => 'https://sandbox.melhorenvio.com.br/oauth/token',
+        'production_token_url' => 'https://www.melhorenvio.com.br/oauth/token',
+    ],
+
+    /*
     | A chave da plataforma é configurada somente por infraestrutura.
     | O onboarding Stripe Connect será dedicado, sem reutilizar OAuth.
     */
