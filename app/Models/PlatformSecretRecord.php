736f9426f836bcdd9508f0721c1b0c4eec18399b
projetosdanhub <projetosdan.hub@gmail.com>
2026-09-08 22:24:18 -0300

    rebase provider platform models onto main

---
+++ app/Models/PlatformSecretRecord.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property string $provider
+ * @property string $purpose
+ * @property string $environment
+ * @property string $status
+ * @property \Illuminate\Support\Carbon|null $rotated_at
+ * @property \Illuminate\Support\Carbon|null $revoked_at
+ */
+class PlatformSecretRecord extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'provider',
+        'purpose',
+        'environment',
+        'secret_ref',
+        'status',
+        'rotated_at',
+        'revoked_at',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'rotated_at' => 'datetime',
+            'revoked_at' => 'datetime',
+        ];
+    }
+}
